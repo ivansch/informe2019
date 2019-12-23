40b9b97be9ce2de207c31bf795a0a304f870c808
ivansch <41492526+ivansch@users.noreply.github.com>
2019-12-23 11:43:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,29 @@ $(function() {
     e.preventDefault();
   });
 });
+$(document).ready(function() {
+  $("#cercle1").mouseover(function() {
+    $("#cercle1").css("background-color", "#00aeef");
+    $("#pathF1").css("fill", "white");
+  });
+  $("#cercle1").mouseout(function() {
+    $("#cercle1").css("background-color", "white");
+    $("#pathF1").css("fill", "#00aeef");
+  });
+  $("#cercle2").mouseover(function() {
+    $("#cercle2").css("background-color", "#00aeef");
+    $("#pathF2").css("fill", "white");
+  });
+  $("#cercle2").mouseout(function() {
+    $("#cercle2").css("background-color", "white");
+    $("#pathF2").css("fill", "#00aeef");
+  });
+  $("#cercle3").mouseover(function() {
+    $("#cercle3").css("background-color", "#00aeef");
+    $("#pathF3").css("fill", "white");
+  });
+  $("#cercle3").mouseout(function() {
+    $("#cercle3").css("background-color", "white");
+    $("#pathF3").css("fill", "#00aeef");
+  });
+});
